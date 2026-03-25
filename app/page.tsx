837bab40ad84f1425e2e4789d1a80d56991768ef
Hazama32 <bintanghack32@gmail.com>
2026-03-25 19:30:40 +0700

    up

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <p>Redirecting to login...</p>
+      <p>Redirecting to login....</p>
     </div>
   );
 }
